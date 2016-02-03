512c296ed1dcf23f2b4d97f909eff2370ca3ba58
stevenedds <stevoeddz@gmail.com>
2016-02-03 17:14:24 -0600

    got steps to display

--- StepCount/ViewController.swift
+++ StepCount/ViewController.swift
@@ -16,39 +16,32 @@ class ViewController: UIViewController {
   
   let healthManager = HealthManager()
   
-  var totalSteps = 0.0 {
-    didSet {
-      stepsLabel.text = "\(totalSteps)"
-    }
-  }
-  
   override func viewDidLoad() {
     super.viewDidLoad()
 
   healthManager.askPermission()
   }
 
-  let completion: ((Double?, NSError?) -> ()) = {
-    (success, error) -> Void in
-    
-    if success == nil {
-      print("You didn't allow HealthKit to access these read/write data types. In your app, try to handle this error gracefully when a user decides not to provide access. The error was: \(error). If you're using a simulator, try it on a device.")
-      return
-    }
-
-    let steps = success!
-    // i'm able to print the number of steps
-    print(steps)
-    
-    dispatch_async(dispatch_get_main_queue()) {
-
-    // i thin the Label should be updated here, but cant figure out how to do it
+  func getSteps() {
+    healthManager.fetchDailyStepsWithCompletionHandler { (steps, error) -> () in
+      if error != nil {
+        print(error?.localizedDescription)
+        
+      } else {
+        
+        if let steps = steps {
+          dispatch_async(dispatch_get_main_queue(), {
+            
+            self.stepsLabel.text = "\(steps)"
+            })
+        }
+      }
     }
   }
-
+  
   @IBAction func getDataButton() {
-    healthManager.fetchDailyStepsWithCompletionHandler(completion)
 
+    getSteps()
   }
   
 }
