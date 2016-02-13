38a41c17da63b5fe18d8b6791b7f5be3200adad5
stevenedds <stevoeddz@gmail.com>
2016-02-13 15:56:06 -0600

    set up dispatch groups

--- StepCount/ChallengeDetailViewController.swift
+++ StepCount/ChallengeDetailViewController.swift
@@ -7,42 +7,103 @@
 //
 
 import UIKit
+import Charts
 
 class ChallengeDetailViewController: UIViewController {
 
   let healthManager = HealthManager()
   
+  var hourlyTimes = HourlyData()
+  
+  @IBOutlet weak var lineChartView: LineChartView!
+  
   @IBOutlet weak var stepsLabel: UILabel!
+  
+  
   
     override func viewDidLoad() {
         super.viewDidLoad()
 
       healthManager.askPermission()
       
-      
+      getSteps()
+
     }
 
   
   @IBAction func getDataButton() {
     
-    getSteps()
+  }
+  
+  @IBAction func graphButton() {
+    
+
   }
   
 
   func getSteps() {
-    healthManager.fetchHourlyStepsWithCompletionHandler { (steps, error) -> () in
+    
+    healthManager.fetchDailyStepsWithCompletionHandler { (daily, error) -> () in
       if error != nil {
         print(error?.localizedDescription)
         
       } else {
-        if let steps = steps {
+        if let dailySteps = daily {
           dispatch_async(dispatch_get_main_queue(), {
             
-            self.stepsLabel.text = "\(steps)"
+            self.stepsLabel.text = "\(dailySteps)"
           })
+          
         }
       }
+      
+      
+      
     }
+    
+    healthManager.fetchHourlyStepsWithCompletionHandler { (hourly, error) -> () in
+      if error != nil {
+        print(error?.localizedDescription)
+        
+      } else {
+        
+        self.hourlyTimes = hourly!
+        
+        let hours = Array(1...24).map { Double($0) }
+        
+        let steps = [self.hourlyTimes.hour1, self.hourlyTimes.hour2, self.hourlyTimes.hour3, self.hourlyTimes.hour4, self.hourlyTimes.hour5, self.hourlyTimes.hour6, self.hourlyTimes.hour7, self.hourlyTimes.hour8, self.hourlyTimes.hour9, self.hourlyTimes.hour10, self.hourlyTimes.hour11, self.hourlyTimes.hour12, self.hourlyTimes.hour13, self.hourlyTimes.hour14, self.hourlyTimes.hour15, self.hourlyTimes.hour16, self.hourlyTimes.hour17, self.hourlyTimes.hour18, self.hourlyTimes.hour19, self.hourlyTimes.hour20, self.hourlyTimes.hour21, self.hourlyTimes.hour22, self.hourlyTimes.hour23, self.hourlyTimes.hour24]
+        
+        self.setChart(hours, values: steps)
+      }
+    }
+  }
+  
+  func setChart(dataPoints: [Double], values: [Double]) {
+    
+    var dataEntries: [ChartDataEntry] = []
+    
+    for i in 0..<dataPoints.count {
+      let dataEntry = ChartDataEntry(value: values[i], xIndex: i)
+      dataEntries.append(dataEntry)
+    }
+    
+    var colors: [UIColor] = []
+    
+    for _ in 0..<dataPoints.count {
+      let red = Double(arc4random_uniform(256))
+      let green = Double(arc4random_uniform(256))
+      let blue = Double(arc4random_uniform(256))
+      
+      let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+      colors.append(color)
+    }
+    
+    
+    
+    let lineChartDataSet = LineChartDataSet(yVals: dataEntries, label: "steps")
+    let lineChartData = LineChartData(xVals: dataPoints, dataSet: lineChartDataSet)
+    lineChartView.data = lineChartData
+    
   }
 
 }
--- StepCount/HealthManager.swift
+++ StepCount/HealthManager.swift
@@ -9,6 +9,10 @@
 import Foundation
 import HealthKit
 
+var GlobalMainQueue: dispatch_queue_t {
+  return dispatch_get_main_queue()
+}
+
 class HealthManager {
   
   let healthStore: HKHealthStore? = {
@@ -68,123 +72,241 @@ class HealthManager {
       healthStore?.executeQuery(query)
   }
   
-  func fetchHourlyStepsWithCompletionHandler (
-    completionHandler:(Double?, NSError?)->()) {
-      
-      var data = HourlyData()
-      
-      let date = NSDate()
-      let cal = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
-      let components = cal.components([.Day , .Month, .Year ], fromDate: date)
-      let newDate = cal.dateFromComponents(components)
-      
-      for hour in 1...24 {
+  
+  func fetchHourlyStepsWithCompletionHandler(completionHandler:(HourlyData?, NSError?)->()) {
+    
+    var data = HourlyData()
+    
+    let date = NSDate()
+    let cal = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
+    let components = cal.components([.Day , .Month, .Year ], fromDate: date)
+    let newDate = cal.dateFromComponents(components)
+    
+    var storedError: NSError!
+    let queryGroup = dispatch_group_create()
+    
+    
+    for hour in 1...24 {
+      if hour == 1 {
         
-        if hour == 1 {
+        let endHour = newDate?.dateByAddingTimeInterval(60*60)
+        let sampleType = HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierStepCount)
+        let predicate = NSPredicate(format: "startDate >= %@ AND startDate < %@" ,newDate!, endHour!)
+        let query = HKStatisticsQuery(quantityType: sampleType!, quantitySamplePredicate: predicate, options: .CumulativeSum) {
+          (query, result, error) in
           
-          let endHour = newDate?.dateByAddingTimeInterval(60*60)
-          let sampleType = HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierStepCount)
+          var totalSteps = 0.0
           
-          let predicate = NSPredicate(format: "startDate >= %@ AND startDate < %@" ,newDate!, endHour!)
-          
-          let query = HKStatisticsQuery(quantityType: sampleType!, quantitySamplePredicate: predicate,
-            options: .CumulativeSum)
-            { (query, result, error) in
-              
-              var totalSteps = 0.0
-              
-              if let sumQuantity = result?.sumQuantity() {
-                totalSteps = sumQuantity.doubleValueForUnit(HKUnit.countUnit())
-
-                data.hour1 = totalSteps
-                print("hour\(hour): \(totalSteps)")
-
-              }
-              completionHandler(totalSteps, nil)
+          if let sumQuantity = result?.sumQuantity() {
+            
+            totalSteps = sumQuantity.doubleValueForUnit(HKUnit.countUnit())
+            
+            data.hour1 = totalSteps
           }
+        }
+        
+        healthStore?.executeQuery(query)
+        
+      } else {
+        
+      }
+      let startHour = newDate?.dateByAddingTimeInterval( Double(hour - 1) * (60*60) )
+      let endHour = newDate?.dateByAddingTimeInterval( Double(hour) * (60*60) )
+      let sampleType = HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierStepCount)
+      
+      let predicate = NSPredicate(format: "startDate >= %@ AND startDate < %@" ,startHour!, endHour!)
+      
+      let query = HKStatisticsQuery(quantityType: sampleType!, quantitySamplePredicate: predicate, options: .CumulativeSum) { (query, result, error) in
+      
+        var totalSteps = 0.0
+        
+
+        if let sumQuantity = result?.sumQuantity() {
           
-          healthStore?.executeQuery(query)
-          
-        } else {
-          
-          let startHour = newDate?.dateByAddingTimeInterval( Double(hour - 1) * (60*60) )
-          let endHour = newDate?.dateByAddingTimeInterval( Double(hour) * (60*60) )
-          let sampleType = HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierStepCount)
-          
-          let predicate = NSPredicate(format: "startDate >= %@ AND startDate < %@" ,startHour!, endHour!)
-          
-          let query = HKStatisticsQuery(quantityType: sampleType!, quantitySamplePredicate: predicate,
-            options: .CumulativeSum)
-            { (query, result, error) in
-              
-              var totalSteps = 0.0
-              
-              if let sumQuantity = result?.sumQuantity() {
-                totalSteps = sumQuantity.doubleValueForUnit(HKUnit.countUnit())
-                
-                print("hour\(hour): \(totalSteps)")
-                
-                switch hour {
-                case 2:
-                  data.hour2 = totalSteps
-                case 3:
-                  data.hour3 = totalSteps
-                case 4:
-                  data.hour4 = totalSteps
-                case 5:
-                  data.hour5 = totalSteps
-                case 6:
-                  data.hour6 = totalSteps
-                case 7:
-                  data.hour7 = totalSteps
-                case 8:
-                  data.hour8 = totalSteps
-                case 9:
-                  data.hour9 = totalSteps
-                case 10:
-                  data.hour10 = totalSteps
-                case 11:
-                  data.hour11 = totalSteps
-                case 12:
-                  data.hour12 = totalSteps
-                case 13:
-                  data.hour13 = totalSteps
-                case 14:
-                  data.hour14 = totalSteps
-                case 15:
-                  data.hour15 = totalSteps
-                case 16:
-                  data.hour16 = totalSteps
-                case 17:
-                  data.hour17 = totalSteps
-                case 18:
-                  data.hour18 = totalSteps
-                case 19:
-                  data.hour19 = totalSteps
-                case 20:
-                  data.hour20 = totalSteps
-                case 21:
-                  data.hour21 = totalSteps
-                case 22:
-                  data.hour22 = totalSteps
-                case 23:
-                  data.hour23 = totalSteps
-                case 24:
-                  data.hour24 = totalSteps
-                default:
-                  break
-                }
-                
-              }
-              completionHandler(totalSteps, nil)
+          totalSteps = sumQuantity.doubleValueForUnit(HKUnit.countUnit())
+          switch hour {
+            
+          case 2:
+            data.hour2 = totalSteps
+          case 3:
+            data.hour3 = totalSteps
+          case 4:
+            data.hour4 = totalSteps
+          case 5:
+            data.hour5 = totalSteps
+          case 6:
+            data.hour6 = totalSteps
+          case 7:
+            data.hour7 = totalSteps
+          case 8:
+            data.hour8 = totalSteps
+          case 9:
+            data.hour9 = totalSteps
+          case 10:
+            data.hour10 = totalSteps
+          case 11:
+            data.hour11 = totalSteps
+          case 12:
+            data.hour12 = totalSteps
+          case 13:
+            data.hour13 = totalSteps
+          case 14:
+            data.hour14 = totalSteps
+          case 15:
+            data.hour15 = totalSteps
+          case 16:
+            data.hour16 = totalSteps
+          case 17:
+            data.hour17 = totalSteps
+          case 18:
+            data.hour18 = totalSteps
+          case 19:
+            data.hour19 = totalSteps
+          case 20:
+            data.hour20 = totalSteps
+          case 21:
+            data.hour21 = totalSteps
+          case 22:
+            data.hour22 = totalSteps
+          case 23:
+            data.hour23 = totalSteps
+          case 24:
+            data.hour24 = totalSteps
+          default:
+            break
           }
-          
-          healthStore?.executeQuery(query)
 
         }
+        dispatch_group_leave(queryGroup)
       }
-      
-      
+
+      dispatch_group_enter(queryGroup)
+
+      healthStore?.executeQuery(query)
+    }
+    dispatch_group_notify(queryGroup, GlobalMainQueue) {
+      completionHandler(data,nil)
+    }
   }
 
+  
+//  func OLDfetchHourlyStepsWithCompletionHandler (completionHandler:(HourlyData?, NSError?)->()) {
+//      
+//      var data = HourlyData()
+//      let date = NSDate()
+//      let cal = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
+//      let components = cal.components([.Day , .Month, .Year ], fromDate: date)
+//      let newDate = cal.dateFromComponents(components)
+//      
+//      var storedError: NSError!
+//      let queryGroup = dispatch_group_create()
+//      
+//      for hour in 1...24 {
+//        
+//        if hour == 1 {
+//          
+//          let endHour = newDate?.dateByAddingTimeInterval(60*60)
+//          let sampleType = HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierStepCount)
+//          let predicate = NSPredicate(format: "startDate >= %@ AND startDate < %@" ,newDate!, endHour!)
+//          let query = HKStatisticsQuery(quantityType: sampleType!, quantitySamplePredicate: predicate, options: .CumulativeSum) {
+//            (query, result, error) in
+//              
+//              var totalSteps = 0.0
+//              
+//              if let sumQuantity = result?.sumQuantity() {
+//                
+//                totalSteps = sumQuantity.doubleValueForUnit(HKUnit.countUnit())
+//                
+//                data.hour1 = totalSteps
+//              }
+//          }
+//          
+//          healthStore?.executeQuery(query)
+//          
+//        } else {
+//          
+//          let startHour = newDate?.dateByAddingTimeInterval( Double(hour - 1) * (60*60) )
+//          
+//          let endHour = newDate?.dateByAddingTimeInterval( Double(hour) * (60*60) )
+//          
+//          let sampleType = HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierStepCount)
+//          
+//          let predicate = NSPredicate(format: "startDate >= %@ AND startDate < %@" ,startHour!, endHour!)
+//          
+//          let query = HKStatisticsQuery(quantityType: sampleType!, quantitySamplePredicate: predicate, options: .CumulativeSum) {
+//            (query, result, error) in
+//              var totalSteps = 0.0
+//              if let sumQuantity = result?.sumQuantity() {
+//                
+//                totalSteps = sumQuantity.doubleValueForUnit(HKUnit.countUnit())
+//                
+//                switch hour {
+//                  
+//                case 2:
+//                  data.hour2 = totalSteps
+//                case 3:
+//                  data.hour3 = totalSteps
+//                case 4:
+//                  data.hour4 = totalSteps
+//                case 5:
+//                  data.hour5 = totalSteps
+//                case 6:
+//                  data.hour6 = totalSteps
+//                case 7:
+//                  data.hour7 = totalSteps
+//                case 8:
+//                  data.hour8 = totalSteps
+//                case 9:
+//                  data.hour9 = totalSteps
+//                case 10:
+//                  data.hour10 = totalSteps
+//                case 11:
+//                  data.hour11 = totalSteps
+//                case 12:
+//                  data.hour12 = totalSteps
+//                case 13:
+//                  data.hour13 = totalSteps
+//                case 14:
+//                  data.hour14 = totalSteps
+//                case 15:
+//                  data.hour15 = totalSteps
+//                case 16:
+//                  data.hour16 = totalSteps
+//                case 17:
+//                  data.hour17 = totalSteps
+//                case 18:
+//                  data.hour18 = totalSteps
+//                case 19:
+//                  data.hour19 = totalSteps
+//                case 20:
+//                  data.hour20 = totalSteps
+//                case 21:
+//                  data.hour21 = totalSteps
+//                case 22:
+//                  data.hour22 = totalSteps
+//                case 23:
+//                  data.hour23 = totalSteps
+//                case 24:
+//                  data.hour24 = totalSteps
+//                default:
+//                  break
+//                }
+//              }
+//            dispatch_group_leave(queryGroup)
+//
+//          }
+//          
+//          dispatch_group_enter(queryGroup)
+//
+//          healthStore?.executeQuery(query)
+//          
+//            completionHandler(data,nil)
+//        }
+//        
+//      }
+//      
+//      
+//    
+//      
+//  }
 }
